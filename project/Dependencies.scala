c83a2bc12a7755b300d8c7d7f6a8a0fbe7d8d3b5
Avinash <avinash.mait@gmail.com>
2017-09-10 14:07:38 +0530

    akka dependency added

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -2,4 +2,9 @@ import sbt._
 
 object Dependencies {
   lazy val scalaTest = "org.scalatest" %% "scalatest" % "3.0.3"
+
+  private lazy val akkaVersion = "2.5.4"
+
+  lazy val akka = "com.typesafe.akka" %% "akka-actor" % akkaVersion
+  lazy val akkaTest = "com.typesafe.akka" %% "akka-testkit" % akkaVersion
 }
